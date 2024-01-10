8ad08a7d09c584d7162591891ff6e94c7861f65f
shaimeur <haimeursaad@gmail.com>
2024-01-10 15:50:08 +0100

    adding the props from the initial state to display it in the Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,37 @@
 import { useEffect } from "react"
 import { connect } from "react-redux"
 
-const Cart = (props) =>{
+const Cart = ({fruits,dispatch,users}) =>{
+
+
     useEffect(()=>{
-        console.log(props)
+        console.log(fruits)
+        console.log(users)
+        console.log(dispatch)
     })
     return(
     <div>
-        <h1>Hello from Cart</h1>
+        <div className="m-5 p-2.5 w-96 border-green-400 rounded bg-gray-600">
+        <h1> MyCart</h1>
+
+        <div className="flex-auto flex-col">
+
+            {fruits.map((item)=>(
+                <div key={item.id} className="w-52 mt-1 p-1 border-orange-400 bg-red-400 rounded">
+                      <span className="inline-block w-16 border-indigo-900"> id: {item.id} {item.fruit} {item.quantity} </span>
+                        </div>
+                ))}
+        </div >
+                </div>
     </div>
     )
 }
 
 const mapStateToProps = (state) =>{
-    return {fruits:state.cart}
+    return {
+        fruits:state.cart,
+        users : state.user
+    }
 }
 
 export default connect(mapStateToProps,null)(Cart)
